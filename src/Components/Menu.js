b37856ca42579435bfe2f0b56ce0e64ae3f802db
Trifle22 <xromchenkov99@gmail.com>
2021-06-15 20:49:02 +0300

    lesson4

---
+++ src/Components/Menu.js
@@ -0,0 +1,44 @@
+import React from 'react'
+import styled from 'styled-components'
+import dbMenu from './DBMenu'
+import { ListItem } from './ListItem'
+import banner from '../images/banner.png';
+
+const MenuStyled = styled.main`
+  background-color: #111;
+  margin-top: 110px;
+`;
+
+const SectionMenu = styled.section`
+  padding: 30px;
+  color: #28B463;
+`
+
+const Banner = styled.div`
+  width: 100%;
+  height: 230px;
+  background-image: url(${banner});
+  background-size: cover;
+  background-position: center;
+  background-repeat: no-repeat;
+`
+
+export const Menu = () => {
+  return (
+    <MenuStyled>
+      {/* banner */}
+      <Banner />
+      <SectionMenu>
+        <h2>Бургеры</h2>
+        <ListItem itemList={dbMenu.burger}/>
+      </SectionMenu>
+      <SectionMenu>
+        <h2>Закуски / Напитки</h2>
+        <ListItem itemList={dbMenu.other
+        }/>
+      </SectionMenu>
+    </MenuStyled>
+
+  ) 
+
+}
